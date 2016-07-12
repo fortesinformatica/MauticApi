3790536cfd1c0bff144d621d75073f098ca35276
joel <joel.r.mz@gmail.com>
2016-07-11 22:00:46 -0300

    Tests in MauticClientConfiguration

---
+++ src/MauticApi.Client.Test/MauticClientConfigurationTest.cs
@@ -0,0 +1,23 @@
+﻿using MauticApi.Client.Models;
+using NUnit.Framework;
+
+namespace MauticApi.Client.Test
+{
+    [TestFixture]
+    public class MauticClientConfigurationTest
+    {
+        [TestCase("http://test.com")]
+        public void WhenCreateAnInstanceWithAPaththatDoesntEndsWithindex_phpAppendItToPath(string path)
+        {
+            var mauticConfig = new MauticClientConfiguration(path);
+            StringAssert.EndsWith("/index.php", mauticConfig.BaseUrl);
+        }
+
+        [TestCase("http://test.com/index.php")]
+        public void WhenCreateAnInstanceWithAPathThatEndsWith_index_phpDoesntAppendItToPath(string path)
+        {
+            var mauticConfig = new MauticClientConfiguration(path);
+            StringAssert.EndsWith(path, mauticConfig.BaseUrl);
+        }
+    }
+}
